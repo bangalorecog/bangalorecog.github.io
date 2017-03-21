fa134fa0a9f6508d86be93bf00fe8e815049c3b9
Alwin Paul <alwinpaul@Alwins-MacBook-Air.local>
2017-03-22 03:02:12 +0530

    google maps added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,40 +1,51 @@
     $(document).ready(function() {
-                $('.slider-container').slick({
-                    accessibility: true,
-                    autoplay: true,
-                    autoplaySpeed: 3000,
-                    arrows: false,
+        $('.slider-container').slick({
+            accessibility: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: false,
+            infinite: true,
+            mobileFirst: true,
+            pauseOnHover: true,
+            dots: true
+        });
+        $('.activity-box-ctr').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows: true,
+            responsive: [{
+                breakpoint: 1028,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                     infinite: true,
-                    mobileFirst: true,
-                    pauseOnHover: true,
-                    dots: true
-                });
-                $('.activity-box-ctr').slick({
-                        infinite: true,
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        arrows: true,
-                        responsive: [{
-                                breakpoint: 1028,
-                                settings: {
-                                    slidesToShow: 2,
-                                    slidesToScroll: 2,
-                                    infinite: true,
-                                }
-                            },
-                            {
-                                breakpoint: 770,
-                                settings: {
-                                    slidesToShow: 1,
-                                    slidesToScroll: 1,
-                                    infinite: true,
-                                }
-                            }]
-                        });
+                }
+            }, {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            }]
+        });
 
-                $('#header_res_bar').on('click',function(){
-                    $('#nav_container').toggleClass('show-menu');
-                })
+        $('#header_res_bar').on('click', function() {
+            $('#nav_container').toggleClass('show-menu');
+        })
 
 
-                });
+    });
+
+    function initMap() {
+        var uluru = { lat: 12.939629, lng: 77.625979 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 14,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
