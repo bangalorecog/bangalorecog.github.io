c3ea2474031c52170223e369a1075a1ac18f8323
Alwin Paul <alwinpaul@Alwins-MacBook-Air.local>
2017-04-08 22:15:17 +0530

    add about,ministry and gallery pages

---
+++ scripts/galley.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+
+    jQuery("#my_nanogallery2").nanogallery2({
+        userID: '153776698@N08',
+        kind: 'flickr',
+        thumbnailHeight: 300,
+        thumbnailWidth: 300,
+        thumbnailGutterWidth: 10,
+        thumbnailGutterHeight: 10
+    });
+
+});
